5864bc814e527955d19a6da1396ee075bb8a30e7
tejaprakashchoudary <tejaprakashchoudary@gmail.com>
2019-01-14 12:21:40 +0530

    Changes made to .graphqls file
    Deleted Department from StudentMapper
    
    changes made to .graphqls schema file

--- src/main/java/com/synectiks/cms/service/mapper/StudentMapper.java
+++ src/main/java/com/synectiks/cms/service/mapper/StudentMapper.java
@@ -8,19 +8,17 @@ import org.mapstruct.*;
 /**
  * Mapper for the entity Student and its DTO StudentDTO.
  */
-@Mapper(componentModel = "spring", uses = {BatchMapper.class, SectionMapper.class, BranchMapper.class, DepartmentMapper.class})
+@Mapper(componentModel = "spring", uses = {BatchMapper.class, SectionMapper.class, BranchMapper.class})
 public interface StudentMapper extends EntityMapper<StudentDTO, Student> {
 
     @Mapping(source = "batch.id", target = "batchId")
     @Mapping(source = "section.id", target = "sectionId")
     @Mapping(source = "branch.id", target = "branchId")
-    @Mapping(source = "department.id", target = "departmentId")
     StudentDTO toDto(Student student);
 
     @Mapping(source = "batchId", target = "batch")
     @Mapping(source = "sectionId", target = "section")
     @Mapping(source = "branchId", target = "branch")
-    @Mapping(source = "departmentId", target = "department")
     Student toEntity(StudentDTO studentDTO);
 
     default Student fromId(Long id) {
