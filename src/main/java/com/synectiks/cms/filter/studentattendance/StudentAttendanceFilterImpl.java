52c6349ca222d349cc3ff179e69dcdfef4b2b2d9
Saurabh <saurabh@bel-technology.com>
2019-01-24 21:39:59 +0530

    StudentAttendance select query updated

--- src/main/java/com/synectiks/cms/filter/studentattendance/StudentAttendanceFilterImpl.java
+++ src/main/java/com/synectiks/cms/filter/studentattendance/StudentAttendanceFilterImpl.java
@@ -1,15 +1,14 @@
 package com.synectiks.cms.filter.studentattendance;
 
     import java.math.BigInteger;
-    import java.util.ArrayList;
-    import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
 
-    import javax.persistence.EntityManager;
-    import javax.persistence.PersistenceContext;
-    import javax.persistence.Query;
-    import java.sql.Date;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
-    import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Repository;
 
 //@Component
 @Repository
@@ -18,38 +17,45 @@ public class StudentAttendanceFilterImpl  {
     @PersistenceContext
     private EntityManager entityManager;
 
-    private String selectQry = "select distinct st.id, st.student_name, " +
-        "(select a.attendance_status from student_attendance a where a.student_id = st.id and a.attendance_date = date(?)) as current_date_status, " +
-        "(select a.attendance_status as prev1_day_status from student_attendance a where a.student_id = st.id and a.attendance_date = date(date(?) - interval '1 day')),   " +
-        "(select a.attendance_status as prev2_day_status from student_attendance a where a.student_id = st.id and a.attendance_date = date(date(?) - interval '2 day')),   " +
-        "(select a.attendance_status as prev3_day_status from student_attendance a where a.student_id = st.id and a.attendance_date = date(date(?) - interval '3 day')),   " +
-        "sa.comments "+
-        "from student st  " +
-        "left join student_attendance sa on st.id = sa.student_id  " +
-        "inner join department dt on st.department_id = dt.id " +
-        "inner join student_subject ss on ss.student_id = st.id " + 
-        "inner join subject sb on ss.subject_id = sb.id " +
-        "where st.branch_id = ? and dt.id = ? and st.batch_id = ? and st.section_id = ? and sb.id = ? ";
+//    private String selectQry = "select distinct st.id, st.student_name, " +
+//        "(select a.attendance_status from student_attendance a where a.student_id = st.id and a.attendance_date = date(?)) as current_date_status, " +
+//        "(select a.attendance_status as prev1_day_status from student_attendance a where a.student_id = st.id and a.attendance_date = date(date(?) - interval '1 day')),   " +
+//        "(select a.attendance_status as prev2_day_status from student_attendance a where a.student_id = st.id and a.attendance_date = date(date(?) - interval '2 day')),   " +
+//        "(select a.attendance_status as prev3_day_status from student_attendance a where a.student_id = st.id and a.attendance_date = date(date(?) - interval '3 day')),   " +
+//        "sa.comments "+
+//        "from student st  " +
+//        "left join student_attendance sa on st.id = sa.student_id  " +
+//        "inner join department dt on st.department_id = dt.id " +
+//        "inner join student_subject ss on ss.student_id = st.id " + 
+//        "inner join subject sb on ss.subject_id = sb.id " +
+//        "where st.branch_id = ? and dt.id = ? and st.batch_id = ? and st.section_id = ? and sb.id = ? ";
 
     public List<DailyAttendanceVo> getStudenceAttendance(StudentAttendanceFilterInput filter) {
+    	String selectQry = "select st.id, st.student_name, sa.attendance_status as current_date_status, " + 
+    			"(select a.attendance_status as prev1_day_status from student_attendance a where a.lecture_id = (select b.id from lecture b where b.attendancemaster_id = (select c.attendancemaster_id from lecture c where c.id = ?) and b.lec_date = date(lc.lec_date) - 1)),  " + 
+    			"(select a.attendance_status as prev2_day_status from student_attendance a where a.lecture_id = (select b.id from lecture b where b.attendancemaster_id = (select c.attendancemaster_id from lecture c where c.id = ?) and b.lec_date = date(lc.lec_date) - 2)),  " + 
+    			"(select a.attendance_status as prev3_day_status from student_attendance a where a.lecture_id = (select b.id from lecture b where b.attendancemaster_id = (select c.attendancemaster_id from lecture c where c.id = ?) and b.lec_date = date(lc.lec_date) - 3)),  " +
+    			"sa.comments "+
+    			"from student st, student_attendance sa, lecture lc " + 
+    			"where st.id = sa.student_id and sa.lecture_id = lc.id  and " + 
+    			"st.branch_id = ? and st.department_id = ? and st.batch_id = ? and st.section_id = ? and sa.lecture_id = ? and lc.lec_date = date(?) ";
         if(filter.getStudentName() != null) {
             selectQry = selectQry + " and st.student_name = ? ";
         }
         Query query = this.entityManager.createNativeQuery(selectQry);
         if(filter != null) {
-            int i=0;
-            //java.sql.Date dt = new java.sql.Date(new java.util.Date(filter.getAttendanceDate()).getTime());
-            query.setParameter(++i, filter.getAttendanceDate());
-            query.setParameter(++i, filter.getAttendanceDate());
-            query.setParameter(++i, filter.getAttendanceDate());
-            query.setParameter(++i, filter.getAttendanceDate());
-            query.setParameter(++i, Integer.parseInt(filter.getBranchId()));
-            query.setParameter(++i, Integer.parseInt(filter.getDepartmentId()));
-            query.setParameter(++i, Integer.parseInt(filter.getBatchId()));
-            query.setParameter(++i, Integer.parseInt(filter.getSectionId()));
-            query.setParameter(++i, Integer.parseInt(filter.getSubjectId()));
+//            int i=0;
+            query.setParameter(1, Integer.parseInt(filter.getLectureId()));
+            query.setParameter(2, Integer.parseInt(filter.getLectureId()));
+            query.setParameter(3, Integer.parseInt(filter.getLectureId()));
+            query.setParameter(4, Integer.parseInt(filter.getBranchId()));
+            query.setParameter(5, Integer.parseInt(filter.getDepartmentId()));
+            query.setParameter(6, Integer.parseInt(filter.getBatchId()));
+            query.setParameter(7, Integer.parseInt(filter.getSectionId()));
+            query.setParameter(8, Integer.parseInt(filter.getLectureId()));
+            query.setParameter(9, filter.getAttendanceDate());
             if(filter.getStudentName() != null) {
-                query.setParameter(++i, filter.getStudentName());
+                query.setParameter(10, filter.getStudentName());
             }
         }
 
@@ -57,11 +63,11 @@ public class StudentAttendanceFilterImpl  {
         return resultList;
     }
 
-    public List<DailyAttendanceVo> getStudenceAttendance() {
-        Query query = this.entityManager.createNativeQuery(selectQry);
-        List<DailyAttendanceVo> resultList = executeSelectQuery(query);
-        return resultList;
-    }
+//    public List<DailyAttendanceVo> getStudenceAttendance() {
+//        Query query = this.entityManager.createNativeQuery(selectQry);
+//        List<DailyAttendanceVo> resultList = executeSelectQuery(query);
+//        return resultList;
+//    }
 
     private List<DailyAttendanceVo> executeSelectQuery(Query query) {
         List<DailyAttendanceVo> resultList = new ArrayList<DailyAttendanceVo>();
