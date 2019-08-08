dd0e84addfbed4d7ec2157242031dbc21699f94f
manojkrjoshi <44704973+manojkrjoshi@users.noreply.github.com>
2019-08-08 15:53:35 +0530

    dateOfBirth issue fixed

---
+++ src/main/java/com/synectiks/cms/domain/CmsStudentVo.java
@@ -0,0 +1,382 @@
+package com.synectiks.cms.domain;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+import org.springframework.data.elasticsearch.annotations.Document;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+import com.synectiks.cms.domain.enumeration.Religion;
+
+import com.synectiks.cms.domain.enumeration.Caste;
+
+import com.synectiks.cms.domain.enumeration.Gender;
+
+import com.synectiks.cms.domain.enumeration.Bloodgroup;
+
+import com.synectiks.cms.domain.enumeration.RelationWithStudentEnum;
+
+import com.synectiks.cms.domain.enumeration.StudentTypeEnum;
+
+/**
+ * A Student.
+ */
+
+public class CmsStudentVo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    private Long id;
+    private String studentName;
+    private String studentMiddleName;
+    private String studentLastName;
+    private String fatherName;
+    private String fatherMiddleName;
+    private String fatherLastName;
+    private String motherName;
+    private String motherMiddleName;
+    private String motherLastName;
+    private Long aadharNo;
+    private LocalDate dateOfBirth;
+    private String placeOfBirth;
+    private Religion religion;
+    private Caste caste;
+    private String subCaste;
+    private Integer age;
+    private Gender sex;
+    private Bloodgroup bloodGroup;
+    private String addressLineOne;
+    private String addressLineTwo;
+    private String addressLineThree;
+    private String town;
+    private String state;
+    private String country;
+    private Long pincode;
+    private String studentContactNumber;
+    private String alternateContactNumber;
+    private String studentEmailAddress;
+    private String alternateEmailAddress;
+    private RelationWithStudentEnum relationWithStudent;
+    private String emergencyContactName;
+    private String emergencyContactMiddleName;
+    private String emergencyContactLastName;
+    private String emergencyContactNo;
+    private String emergencyContactEmailAddress;
+    private String uploadPhoto;
+    private Long admissionNo;
+    private String rollNo;
+    private StudentTypeEnum studentType;
+    private Department department;
+    private Batch batch;
+    private Section section;
+    private Branch branch;
+    private String strDateOfBirth;
+    private Long departmentId;
+    private Long batchId;
+    private Long sectionId;
+    private Long branchId;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public String getStudentMiddleName() {
+		return studentMiddleName;
+	}
+	public void setStudentMiddleName(String studentMiddleName) {
+		this.studentMiddleName = studentMiddleName;
+	}
+	public String getStudentLastName() {
+		return studentLastName;
+	}
+	public void setStudentLastName(String studentLastName) {
+		this.studentLastName = studentLastName;
+	}
+	public String getFatherName() {
+		return fatherName;
+	}
+	public void setFatherName(String fatherName) {
+		this.fatherName = fatherName;
+	}
+	public String getFatherMiddleName() {
+		return fatherMiddleName;
+	}
+	public void setFatherMiddleName(String fatherMiddleName) {
+		this.fatherMiddleName = fatherMiddleName;
+	}
+	public String getFatherLastName() {
+		return fatherLastName;
+	}
+	public void setFatherLastName(String fatherLastName) {
+		this.fatherLastName = fatherLastName;
+	}
+	public String getMotherName() {
+		return motherName;
+	}
+	public void setMotherName(String motherName) {
+		this.motherName = motherName;
+	}
+	public String getMotherMiddleName() {
+		return motherMiddleName;
+	}
+	public void setMotherMiddleName(String motherMiddleName) {
+		this.motherMiddleName = motherMiddleName;
+	}
+	public String getMotherLastName() {
+		return motherLastName;
+	}
+	public void setMotherLastName(String motherLastName) {
+		this.motherLastName = motherLastName;
+	}
+	public Long getAadharNo() {
+		return aadharNo;
+	}
+	public void setAadharNo(Long aadharNo) {
+		this.aadharNo = aadharNo;
+	}
+	public LocalDate getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(LocalDate dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public String getPlaceOfBirth() {
+		return placeOfBirth;
+	}
+	public void setPlaceOfBirth(String placeOfBirth) {
+		this.placeOfBirth = placeOfBirth;
+	}
+	public Religion getReligion() {
+		return religion;
+	}
+	public void setReligion(Religion religion) {
+		this.religion = religion;
+	}
+	public Caste getCaste() {
+		return caste;
+	}
+	public void setCaste(Caste caste) {
+		this.caste = caste;
+	}
+	public String getSubCaste() {
+		return subCaste;
+	}
+	public void setSubCaste(String subCaste) {
+		this.subCaste = subCaste;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public Gender getSex() {
+		return sex;
+	}
+	public void setSex(Gender sex) {
+		this.sex = sex;
+	}
+	public Bloodgroup getBloodGroup() {
+		return bloodGroup;
+	}
+	public void setBloodGroup(Bloodgroup bloodGroup) {
+		this.bloodGroup = bloodGroup;
+	}
+	public String getAddressLineOne() {
+		return addressLineOne;
+	}
+	public void setAddressLineOne(String addressLineOne) {
+		this.addressLineOne = addressLineOne;
+	}
+	public String getAddressLineTwo() {
+		return addressLineTwo;
+	}
+	public void setAddressLineTwo(String addressLineTwo) {
+		this.addressLineTwo = addressLineTwo;
+	}
+	public String getAddressLineThree() {
+		return addressLineThree;
+	}
+	public void setAddressLineThree(String addressLineThree) {
+		this.addressLineThree = addressLineThree;
+	}
+	public String getTown() {
+		return town;
+	}
+	public void setTown(String town) {
+		this.town = town;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public Long getPincode() {
+		return pincode;
+	}
+	public void setPincode(Long pincode) {
+		this.pincode = pincode;
+	}
+	public String getStudentContactNumber() {
+		return studentContactNumber;
+	}
+	public void setStudentContactNumber(String studentContactNumber) {
+		this.studentContactNumber = studentContactNumber;
+	}
+	public String getAlternateContactNumber() {
+		return alternateContactNumber;
+	}
+	public void setAlternateContactNumber(String alternateContactNumber) {
+		this.alternateContactNumber = alternateContactNumber;
+	}
+	public String getStudentEmailAddress() {
+		return studentEmailAddress;
+	}
+	public void setStudentEmailAddress(String studentEmailAddress) {
+		this.studentEmailAddress = studentEmailAddress;
+	}
+	public String getAlternateEmailAddress() {
+		return alternateEmailAddress;
+	}
+	public void setAlternateEmailAddress(String alternateEmailAddress) {
+		this.alternateEmailAddress = alternateEmailAddress;
+	}
+	public RelationWithStudentEnum getRelationWithStudent() {
+		return relationWithStudent;
+	}
+	public void setRelationWithStudent(RelationWithStudentEnum relationWithStudent) {
+		this.relationWithStudent = relationWithStudent;
+	}
+	public String getEmergencyContactName() {
+		return emergencyContactName;
+	}
+	public void setEmergencyContactName(String emergencyContactName) {
+		this.emergencyContactName = emergencyContactName;
+	}
+	public String getEmergencyContactMiddleName() {
+		return emergencyContactMiddleName;
+	}
+	public void setEmergencyContactMiddleName(String emergencyContactMiddleName) {
+		this.emergencyContactMiddleName = emergencyContactMiddleName;
+	}
+	public String getEmergencyContactLastName() {
+		return emergencyContactLastName;
+	}
+	public void setEmergencyContactLastName(String emergencyContactLastName) {
+		this.emergencyContactLastName = emergencyContactLastName;
+	}
+	public String getEmergencyContactNo() {
+		return emergencyContactNo;
+	}
+	public void setEmergencyContactNo(String emergencyContactNo) {
+		this.emergencyContactNo = emergencyContactNo;
+	}
+	public String getEmergencyContactEmailAddress() {
+		return emergencyContactEmailAddress;
+	}
+	public void setEmergencyContactEmailAddress(String emergencyContactEmailAddress) {
+		this.emergencyContactEmailAddress = emergencyContactEmailAddress;
+	}
+	public String getUploadPhoto() {
+		return uploadPhoto;
+	}
+	public void setUploadPhoto(String uploadPhoto) {
+		this.uploadPhoto = uploadPhoto;
+	}
+	public Long getAdmissionNo() {
+		return admissionNo;
+	}
+	public void setAdmissionNo(Long admissionNo) {
+		this.admissionNo = admissionNo;
+	}
+	public String getRollNo() {
+		return rollNo;
+	}
+	public void setRollNo(String rollNo) {
+		this.rollNo = rollNo;
+	}
+	public StudentTypeEnum getStudentType() {
+		return studentType;
+	}
+	public void setStudentType(StudentTypeEnum studentType) {
+		this.studentType = studentType;
+	}
+	public Department getDepartment() {
+		return department;
+	}
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+	public Batch getBatch() {
+		return batch;
+	}
+	public void setBatch(Batch batch) {
+		this.batch = batch;
+	}
+	public Section getSection() {
+		return section;
+	}
+	public void setSection(Section section) {
+		this.section = section;
+	}
+	public Branch getBranch() {
+		return branch;
+	}
+	public void setBranch(Branch branch) {
+		this.branch = branch;
+	}
+	public String getStrDateOfBirth() {
+		return strDateOfBirth;
+	}
+	public void setStrDateOfBirth(String strDateOfBirth) {
+		this.strDateOfBirth = strDateOfBirth;
+	}
+	public Long getDepartmentId() {
+		return departmentId;
+	}
+	public void setDepartmentId(Long departmentId) {
+		this.departmentId = departmentId;
+	}
+	public Long getBatchId() {
+		return batchId;
+	}
+	public void setBatchId(Long batchId) {
+		this.batchId = batchId;
+	}
+	public Long getSectionId() {
+		return sectionId;
+	}
+	public void setSectionId(Long sectionId) {
+		this.sectionId = sectionId;
+	}
+	public Long getBranchId() {
+		return branchId;
+	}
+	public void setBranchId(Long branchId) {
+		this.branchId = branchId;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+}
