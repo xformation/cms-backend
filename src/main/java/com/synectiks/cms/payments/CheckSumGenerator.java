f77d2c315772bad01255ef8c770a6d4d9f38a80d
manojkrjoshi <44704973+manojkrjoshi@users.noreply.github.com>
2019-08-14 11:31:45 +0530

    checksum generator code change

--- src/main/java/com/synectiks/cms/payments/CheckSumGenerator.java
+++ src/main/java/com/synectiks/cms/payments/CheckSumGenerator.java
@@ -16,16 +16,18 @@ public final class CheckSumGenerator {
 	private static final String HASH_KEY = "uIZ2iayX70hc";
 	private static final String HASH_ALGO = "HmacSHA256";
 	
-//	public static void main(String a[]) throws NoSuchAlgorithmException, InvalidKeyException {
+	public static void main(String a[]) throws NoSuchAlgorithmException, InvalidKeyException {
 //		String checkSum = getHash("ac");
-//		System.out.println(checkSum);
-//	}
+		String m = getCheckSum("HMACUAT|SYN220191414051400043|NA|6500|NA|NA|NA|INR|NA|R|hmacuat|NA|NA|F|NA|NA|NA|NA|NA|NA|NA|http://18.209.4.2:3000/payment-response");
+		System.out.println(m);
+	}
 	
 	public static synchronized String getCheckSum(String input) throws NoSuchAlgorithmException, InvalidKeyException {
 		Mac sha256_HMAC = Mac.getInstance(HASH_ALGO);
 		SecretKeySpec secret_key = new SecretKeySpec(HASH_KEY.getBytes(), HASH_ALGO);
 		sha256_HMAC.init(secret_key);
-		String hash = Base64.encodeBase64String(sha256_HMAC.doFinal(input.getBytes()));
+		byte[] result = sha256_HMAC.doFinal(input.getBytes());
+		String hash = DatatypeConverter.printHexBinary(result);
 	    return hash.toUpperCase(); 
 	}
 	
