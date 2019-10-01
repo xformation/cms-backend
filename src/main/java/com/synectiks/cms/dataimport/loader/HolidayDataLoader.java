19de76e8683ef1591d1712c2818877486e1c44c8
manojkrjoshi <44704973+manojkrjoshi@users.noreply.github.com>
2019-10-01 12:20:33 +0530

    duplicate holiday find logic changed

--- src/main/java/com/synectiks/cms/dataimport/loader/HolidayDataLoader.java
+++ src/main/java/com/synectiks/cms/dataimport/loader/HolidayDataLoader.java
@@ -35,60 +35,57 @@ public class HolidayDataLoader extends DataLoader {
         StringBuilder sb = new StringBuilder();
         Holiday obj = CommonUtil.createCopyProperties(cls.newInstance(), Holiday.class);
 
-        obj.setHolidayStatus(Status.ACTIVE);
-        if (!this.allRepositories.findRepository(this.sheetName).exists(Example.of(obj))) {
-
-            String holidayDesc = row.getCellAsString(0).orElse(null);
-            if (CommonUtil.isNullOrEmpty(holidayDesc)) {
-                sb.append("holiday_desc, ");
-                logger.warn("Mandatory field missing. Field name - holiday_desc");
-            } else {
-                obj.setHolidayDesc(holidayDesc);
-            }
-
-
-            String holidayDate = row.getCellAsString(1).orElse(null);
-            if (CommonUtil.isNullOrEmpty(holidayDate)) {
-                sb.append("holiday_date, ");
-                logger.warn("Mandatory field missing. Field name - holiday_date");
-            } else {
-                obj.holidayDate(DateFormatUtil.convertStringToLocalDate(holidayDate, CmsConstants.DATE_FORMAT_dd_MM_yyyy));
-            }
-
-
-            String termStatus = row.getCellAsString(3).orElse(null);
-            if (CommonUtil.isNullOrEmpty(termStatus)) {
-                sb.append("status, ");
-                logger.warn("Mandatory field missing. Field name - term_status");
-            } else {
-                if (Status.ACTIVE.toString().equalsIgnoreCase(termStatus)) {
-                    obj.setHolidayStatus(Status.ACTIVE);
-                } else {
-                    obj.setHolidayStatus(Status.DEACTIVE);
-                }
-            }
-        }else {
-            sb.append("Application already have an active holiday status, ");
-            logger.warn("Application already have an active holiday status");
-            if (sb.length() > 0) {
-                String msg = "Application already have an active holiday status";
-                throw new AdditionalHolidayFoundException(msg);
-            }
-        }
-
         String academicYear = row.getCellAsString(4).orElse(null);
         if(CommonUtil.isNullOrEmpty(academicYear)) {
             sb.append("jhi_year, ");
             logger.warn("Mandatory field missing. Field name - jhi_year");
         }else {
-            AcademicYear academicYear1 = new AcademicYear();
-            academicYear1.setYear(academicYear);
-            Optional<AcademicYear> ay = this.allRepositories.findRepository("academic_year").findOne(Example.of(academicYear1));
-            if(ay.isPresent()) {
-                obj.setAcademicyear(ay.get());
+            AcademicYear ay = new AcademicYear();
+            ay.setYear(academicYear);
+            Optional<AcademicYear> oay = this.allRepositories.findRepository("academic_year").findOne(Example.of(ay));
+            if(oay.isPresent()) {
+                obj.setAcademicyear(oay.get());
             }else {
-                sb.append("jhi_year_id, ");
+                sb.append("jhi_year, ");
                 logger.warn("AcademicYear not found. Given academicYear name : "+academicYear);
+            }
+        }
+        
+        String holidayDesc = row.getCellAsString(0).orElse(null);
+        if (CommonUtil.isNullOrEmpty(holidayDesc)) {
+            sb.append("holiday_desc, ");
+            logger.warn("Mandatory field missing. Field name - holiday_desc");
+        } else {
+            obj.setHolidayDesc(holidayDesc);
+        }
+
+        String holidayDate = row.getCellAsString(1).orElse(null);
+        if (CommonUtil.isNullOrEmpty(holidayDate)) {
+            sb.append("holiday_date, ");
+            logger.warn("Mandatory field missing. Field name - holiday_date");
+        } else {
+            obj.holidayDate(DateFormatUtil.convertStringToLocalDate(holidayDate, CmsConstants.DATE_FORMAT_dd_MM_yyyy));
+        }
+
+        if(!CommonUtil.isNullOrEmpty(academicYear) && !CommonUtil.isNullOrEmpty(holidayDesc) && !CommonUtil.isNullOrEmpty(holidayDate) 
+        		&& this.allRepositories.findRepository(this.sheetName).exists(Example.of(obj))) {
+        	String msg = "Application already have a holiday with name : "+holidayDesc+" for the date : "+holidayDate+" in the given academic year :"+academicYear;
+        	sb.append(msg);
+        	logger.warn(msg);
+        	if (sb.length() > 0) {
+              throw new AdditionalHolidayFoundException(msg);
+        	}
+        }
+        
+        String holidayStatus = row.getCellAsString(3).orElse(null);
+        if (CommonUtil.isNullOrEmpty(holidayStatus)) {
+            sb.append("status, ");
+            logger.warn("Mandatory field missing. Field name - holiday_status");
+        } else {
+            if (Status.ACTIVE.toString().equalsIgnoreCase(holidayStatus)) {
+                obj.setHolidayStatus(Status.ACTIVE);
+            } else {
+                obj.setHolidayStatus(Status.DEACTIVE);
             }
         }
 
